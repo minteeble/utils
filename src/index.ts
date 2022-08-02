9adfc6f84ebb22fd3b14cf6bbb534e2304678043
Samuele Girgenti <samuele.girgenti458@gmail.com>
2022-08-02 06:34:25 +0200

    Added DTO for creating reserved area metadata

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { TokenBalanceResponseDto } from "./models/dtos/response/tokenBalanceResponseDto";
 export { NftsBalanceResponseDto } from "./models/dtos/response/nftBalanceResponseDto";
+export { CreateReservedAreaRequestDto } from "./models/dtos/request/createReservedAreaRequestDto";
---
+++ src/models/dtos/request/createReservedAreaRequestDto.ts
@@ -0,0 +1,5 @@
+export interface CreateReservedAreaRequestDto {
+  urlName: string;
+
+  fullName: string;
+}
