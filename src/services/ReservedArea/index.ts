861b6cc6165f825bcebacf154e8fac38d19d2193
Samuele Girgenti <samuele.girgenti458@gmail.com>
2022-08-04 01:17:05 +0200

    added dto

--- src/services/ReservedArea/index.ts
+++ src/services/ReservedArea/index.ts
@@ -1,1 +1,4 @@
 export * from "./models/createReservedAreaRequestDto";
+export * from "./models/getUserAccessInfoRequestDto";
+export * from "./models/getUserAccessInfoResponseDto";
+export * from "./models/getUserAccessInfoResponseDto";
