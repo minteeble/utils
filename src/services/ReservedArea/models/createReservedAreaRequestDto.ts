32f208bcfa26fee91dfc12a721ec29fe83d99ead
Samuele Girgenti <samuele.girgenti458@gmail.com>
2022-08-03 04:24:20 +0200

    Update config

--- src/services/ReservedArea/models/createReservedAreaRequestDto.ts
+++ src/services/ReservedArea/models/createReservedAreaRequestDto.ts
@@ -7,6 +7,12 @@ export interface ICreateReservedAreaRequestDto extends IBaseModel {
   fullName: string;
 
   owner: string;
+
+  contractAddress: string;
+
+  chain: string;
+
+  type: string;
 }
 
 @JsonObject()
@@ -14,14 +20,23 @@ export class CreateReservedAreaRequestDto
   extends BaseModel
   implements ICreateReservedAreaRequestDto
 {
-  @JsonProperty({ required: false })
+  @JsonProperty({ required: true })
   urlName: string;
 
-  @JsonProperty({ required: false })
+  @JsonProperty({ required: true })
   fullName: string;
 
   @JsonProperty({ required: false })
   owner: string;
+
+  @JsonProperty({ required: true })
+  contractAddress: string;
+
+  @JsonProperty({ required: true })
+  chain: string;
+
+  @JsonProperty({ required: true })
+  type: string;
 
   constructor() {
     super();
