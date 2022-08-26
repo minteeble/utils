cabdcf5e14329bad3e43aa86d6e05881246b3ad1
Samuele Girgenti <samuele.girgenti458@gmail.com>
2022-08-26 06:11:30 +0200

    update dto

--- src/services/Authorization/Navigation/models/getNavigationByGroupResponseDto.ts
+++ src/services/Authorization/Navigation/models/getNavigationByGroupResponseDto.ts
@@ -1,11 +1,26 @@
-import { IBaseModel } from "../../../../shared";
+import { JsonObject, JsonProperty } from "typescript-json-serializer";
+import { BaseModel, IBaseModel } from "../../../../shared";
 import {
   INavigationCategoryClientModel,
   INavigationExtensionClientModel,
+  NavigationCategoryClientModel,
+  NavigationExtensionClientModel,
 } from "../../shared";
 
 export interface IGetNavigationByGroupResponseDto extends IBaseModel {
   categories: Array<INavigationCategoryClientModel>;
 
   extensions: Array<INavigationExtensionClientModel>;
+}
+
+@JsonObject()
+export class GetNavigationByGroupResponseDto
+  extends BaseModel
+  implements IGetNavigationByGroupResponseDto
+{
+  @JsonProperty({ type: NavigationCategoryClientModel })
+  categories: Array<NavigationCategoryClientModel>;
+
+  @JsonProperty({ type: NavigationExtensionClientModel })
+  extensions: Array<NavigationExtensionClientModel>;
 }
