971120e30bcb64932bd3362bcf5f8b8bd3087ed9
Samuele Girgenti <samuele.girgenti458@gmail.com>
2022-09-26 18:21:41 +0200

    Added base RequestDTO model

--- src/shared/models/RequestDto.ts
+++ src/shared/models/RequestDto.ts
@@ -1,3 +1,12 @@
+import { JsonObject, JsonProperty } from "typescript-json-serializer";
+import { BaseModel } from "./BaseModel";
 import { IDtoModel } from "./DtoModel";
 
 export interface IRequestDto extends IDtoModel {}
+
+@JsonObject()
+export class RequestDto extends BaseModel implements IRequestDto {
+  isValid(): boolean {
+    return true;
+  }
+}
