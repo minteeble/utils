aeb463eec3fbfeb0302d6bd4e647bd1e8449d822
Samuele Girgenti <samuele.girgenti458@gmail.com>
2022-09-26 03:42:33 +0200

    update

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -2,6 +2,8 @@ export * from "./models/BaseModel";
 export * from "./models/SingleOwenableClientModel";
 export * from "./models/SingleOwnable";
 export * from "./models/ValidatableModel";
+export * from "./models/RequestDto";
+export * from "./models/DtoModel";
 
 //TODO fix
 export * from "./nftBalanceResponseDto";
