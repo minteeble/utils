16d15bda7292ec674367e5df45753471f474305e
Samuele Girgenti <samuele.girgenti458@gmail.com>
2022-09-26 22:29:11 +0200

    Release 1.0.43

--- src/services/Shared/WebhookListener/index.ts
+++ src/services/Shared/WebhookListener/index.ts
@@ -1,2 +1,4 @@
 export * from "./models/CreateWebhookListenerRequestDto";
 export * from "./models/CreateWebhookListenerResponseDto";
+export * from "./models/WebhookListenerClientModel";
+export * from "./models/GetListenersResponseDto";
---
+++ src/services/Shared/WebhookListener/models/WebhookListenerClientModel.ts
@@ -0,0 +1,30 @@
+import { JsonObject, JsonProperty } from "typescript-json-serializer";
+import { BaseModel, IBaseModel } from "../../../../shared";
+
+export interface IWebhookListenerClientModel extends IBaseModel {
+  name: string;
+
+  viewers: Array<string>;
+
+  id: string;
+
+  owner: string;
+}
+
+@JsonObject()
+export class WebhookListenerClientModel
+  extends BaseModel
+  implements IWebhookListenerClientModel
+{
+  @JsonProperty()
+  name: string;
+
+  @JsonProperty()
+  viewers: Array<string>;
+
+  @JsonProperty()
+  id: string;
+
+  @JsonProperty()
+  owner: string;
+}
