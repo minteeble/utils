b76f902588371469a1580d8ff6297cf2b17e44f2
Samuele Girgenti <samuele.girgenti458@gmail.com>
2022-09-27 13:59:34 +0200

    Release 1.0.46

---
+++ src/services/Shared/WebhookListener/models/GetWebhookEventsResponseDto.ts
@@ -0,0 +1,16 @@
+import { JsonObject, JsonProperty } from "typescript-json-serializer";
+import { BaseModel, IBaseModel, IRequestDto } from "../../../../shared";
+import { WebhookEventClientModel } from "./WebhookEventClientModel";
+
+export interface IGetWebhookEventsResponseDto extends IBaseModel {
+  events: Array<WebhookEventClientModel>;
+}
+
+@JsonObject()
+export class GetWebhookEventsResponseDto
+  extends BaseModel
+  implements IGetWebhookEventsResponseDto
+{
+  @JsonProperty({ type: WebhookEventClientModel })
+  events: Array<WebhookEventClientModel>;
+}
---
+++ src/services/Shared/WebhookListener/models/WebhookEventClientModel.ts
@@ -0,0 +1,35 @@
+import { JsonObject, JsonProperty } from "typescript-json-serializer";
+import { BaseModel, IBaseModel } from "../../../../shared";
+
+export interface IWebhookEventClientModel extends IBaseModel {
+  method: string;
+
+  payload: any;
+
+  id: string;
+
+  listenerId: string;
+
+  timestamp: string;
+}
+
+@JsonObject()
+export class WebhookEventClientModel
+  extends BaseModel
+  implements IWebhookEventClientModel
+{
+  @JsonProperty()
+  method: string;
+
+  @JsonProperty()
+  payload: any;
+
+  @JsonProperty()
+  id: string;
+
+  @JsonProperty()
+  listenerId: string;
+
+  @JsonProperty()
+  timestamp: string;
+}
