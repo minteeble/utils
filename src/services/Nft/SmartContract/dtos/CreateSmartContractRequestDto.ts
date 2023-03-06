1492b52949aa577925bdc70dd610b9d9406ad1af
Samuele Girgenti <samuele.girgenti458@gmail.com>
2023-03-06 11:56:33 +0100

    Fix request dto

--- src/services/Nft/SmartContract/dtos/CreateSmartContractRequestDto.ts
+++ src/services/Nft/SmartContract/dtos/CreateSmartContractRequestDto.ts
@@ -1,7 +1,26 @@
+import { JsonObject, JsonProperty } from "typescript-json-serializer";
 import { BaseModel, IBaseModel, RequestDto } from "../../../../shared";
 
-export interface ICreateSmartContractRequestDto extends IBaseModel {}
+export interface ICreateSmartContractRequestDto extends IBaseModel {
+  /**
+   * Network chain name
+   */
+  chainName: string;
 
+  /**
+   * Smart contract address
+   */
+  address: string;
+}
+
+@JsonObject()
 export class CreateSmartContractRequestDto
   extends RequestDto
-  implements ICreateSmartContractRequestDto {}
+  implements ICreateSmartContractRequestDto
+{
+  @JsonProperty()
+  address: string;
+
+  @JsonProperty()
+  chainName: string;
+}
