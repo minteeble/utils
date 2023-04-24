9928ff277b3ad158ad95f9407f70c9c963b3dc0c
Samuele Girgenti <samuele.girgenti458@gmail.com>
2023-04-24 13:36:41 +0300

    Renderer dto

--- src/services/Nft/Renderer/dtos/CreateRendererRequestDto.ts
+++ src/services/Nft/Renderer/dtos/CreateRendererRequestDto.ts
@@ -1,9 +1,27 @@
-import { JsonObject } from "typescript-json-serializer";
+import { JsonObject, JsonProperty } from "typescript-json-serializer";
 import { BaseModel, IBaseModel, RequestDto } from "../../../../shared";
+import { NftRendererType } from "../models";
 
-export interface ICreateRendererRequestDto extends IBaseModel {}
+export interface ICreateRendererRequestDto extends IBaseModel {
+  /**
+   * NftRenderer Type
+   */
+  type: NftRendererType;
+
+  /**
+   * Other attributes
+   */
+  attributes: { [key: string]: string };
+}
 
 @JsonObject()
 export class CreateRendererRequestDto
   extends RequestDto
-  implements ICreateRendererRequestDto {}
+  implements ICreateRendererRequestDto
+{
+  @JsonProperty()
+  type: NftRendererType;
+
+  @JsonProperty()
+  attributes: { [key: string]: string };
+}
--- src/services/Nft/Renderer/models/RendererDataClientModel.ts
+++ src/services/Nft/Renderer/models/RendererDataClientModel.ts
@@ -1,9 +1,57 @@
-import { JsonObject } from "typescript-json-serializer";
+import { JsonObject, JsonProperty } from "typescript-json-serializer";
 import { BaseModel, IBaseModel } from "../../../../shared";
 
-export interface IRendererDataClientModel extends IBaseModel {}
+/**
+ * NftRenderer type
+ */
+export enum NftRendererType {
+  DEFAULT = "DEFAULT",
+  LAYERS = "LAYERS",
+  ITEMS = "ITEMS",
+}
+
+export interface IRendererDataClientModel extends IBaseModel {
+  /**
+   * NftRenderer ID
+   */
+  id: string;
+
+  /**
+   * NftRenderer Type
+   */
+  type: NftRendererType;
+
+  /**
+   * Other attributes
+   */
+  attributes: { [key: string]: string };
+}
 
 @JsonObject()
 export class RendererDataClientModel
   extends BaseModel
-  implements IRendererDataClientModel {}
+  implements IRendererDataClientModel
+{
+  @JsonProperty({ required: true })
+  id: string;
+
+  @JsonProperty({ required: true })
+  type: NftRendererType;
+
+  @JsonProperty()
+  attributes: { [key: string]: any };
+
+  /**
+   *
+   * @returns The raw version of renderer data, that can
+   * be used later inside NftRenderFactory for determining the correct NftRender instance
+   */
+  public getRawRendererData(): { [key: string]: any } {
+    let object = {
+      id: this.id,
+      type: this.type,
+    };
+
+    return Object.assign(object, this.attributes);
+  }
+}
