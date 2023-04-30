8ac3eb9230476cab60a147ab3521845bf4d5572f
GiveMeACent <anto.coltry@gmail.com>
2023-04-30 18:47:37 +0200

    Update GetGenerationRequestDto

--- src/services/Nft/Renderer/dtos/GetGenerationRequestDto.ts
+++ src/services/Nft/Renderer/dtos/GetGenerationRequestDto.ts
@@ -1,11 +1,8 @@
 import { JsonObject, JsonProperty } from "typescript-json-serializer";
 import { IBaseModel, RequestDto } from "../../../../shared";
-import { NftGenerationType } from "../models";
 
 export interface IGetGenerationRequestDto extends IBaseModel {
   generationId: string;
-
-  type: NftGenerationType;
 }
 
 @JsonObject()
@@ -15,7 +12,4 @@ export class GetGenerationRequestDto
 {
   @JsonProperty()
   generationId: string;
-
-  @JsonProperty()
-  type: NftGenerationType;
 }
