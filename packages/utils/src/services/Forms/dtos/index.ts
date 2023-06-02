6c60f93d26e7656df69681a7a0133bfef146fe98
Samuele Girgenti <samuele.girgenti458@gmail.com>
2023-06-02 18:04:36 +0200

    Init FormAnswer models/dtos

--- packages/utils/src/services/Forms/dtos/index.ts
+++ packages/utils/src/services/Forms/dtos/index.ts
@@ -3,3 +3,4 @@ export * from "./GetFormsResponseDto";
 export * from "./CreateFormRequestDto";
 export * from "./GetFormRequestDto";
 export * from "./GetFormResponseDto";
+export * from "./SendFormAnswerRequestDto";
--- packages/utils/src/services/Forms/models/index.ts
+++ packages/utils/src/services/Forms/models/index.ts
@@ -1,1 +1,2 @@
 export * from "./FormClientModel";
+export * from "./FormAnswerClientModel";
