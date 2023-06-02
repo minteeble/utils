3ef4268cf3c4f8793a5d84c01d1da34067dee354
NovaExee <ciolinofederico@gmail.com>
2023-06-02 19:56:49 +0200

    init forms query logic

--- packages/utils/src/services/Forms/dtos/index.ts
+++ packages/utils/src/services/Forms/dtos/index.ts
@@ -3,4 +3,6 @@ export * from "./GetFormsResponseDto";
 export * from "./CreateFormRequestDto";
 export * from "./GetFormRequestDto";
 export * from "./GetFormResponseDto";
+export * from "./GetFormAnswersRequestDto";
+export * from "./GetFormAnswersResponseDto";
 export * from "./SendFormAnswerRequestDto";
