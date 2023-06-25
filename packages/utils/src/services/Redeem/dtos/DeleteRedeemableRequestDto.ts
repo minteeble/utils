4c95ae3982cab70ee5c27d9253912fb2794fa3e0
GiveMeACent <anto.coltry@gmail.com>
2023-06-25 23:43:24 +0200

    update property name

--- packages/utils/src/services/Redeem/dtos/DeleteRedeemableRequestDto.ts
+++ packages/utils/src/services/Redeem/dtos/DeleteRedeemableRequestDto.ts
@@ -3,7 +3,7 @@ import { JsonProperty, JsonObject } from "typescript-json-serializer";
 
 export interface IDeleteRedeemableRequestDto extends IBaseModel {
   id: string;
-  redeemConfigId: string;
+  configId: string;
 }
 
 @JsonObject()
@@ -15,5 +15,5 @@ export class DeleteRedeemableRequestDto
   id: string;
 
   @JsonProperty()
-  redeemConfigId: string;
+  configId: string;
 }
