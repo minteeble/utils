c0a3a24df18bd98e1c6a65254b2f2b30a0f2aef5
Samuele Girgenti <samuele.girgenti458@gmail.com>
2023-07-04 17:46:22 +0200

    VisibilityPolicyUtils

--- packages/utils/src/models/VisibilityPolicy.ts
+++ packages/utils/src/models/VisibilityPolicy.ts
@@ -17,3 +17,25 @@ export enum VisibilityPolicy {
    */
   ReadWrite,
 }
+
+/**
+ * Utils methods for validating visibility policies
+ */
+export class VisibilityPolicyUtils {
+  /**
+   * Checks that provided policy has at least read permissions
+   *
+   * @param policy Visibility policy to be validated
+   * @returns
+   */
+  public static hasAtLeastReadPermission(policy: VisibilityPolicy) {
+    return (
+      policy === VisibilityPolicy.ReadOnly ||
+      policy === VisibilityPolicy.ReadWrite
+    );
+  }
+
+  public static hasReadWritePermission(policy: VisibilityPolicy) {
+    return policy === VisibilityPolicy.ReadWrite;
+  }
+}
