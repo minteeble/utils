13bc87d477c93a1b2b2c0f5e34c06d80fe62a5e6
Samuele Girgenti <samuele.girgenti458@gmail.com>
2023-07-25 18:17:57 +0200

    NftGeneration variables

--- packages/utils/src/services/Renderer/models/GenerationDataClientModel.ts
+++ packages/utils/src/services/Renderer/models/GenerationDataClientModel.ts
@@ -8,6 +8,42 @@ export enum NftGenerationType {
   STATIC_MUTANT = "STATIC_MUTANT",
 }
 
+/**
+ * Stages where variable resolution has to take place
+ */
+export enum NftGenerationResolutionStage {
+  /**
+   * Resolves variable during the rendering process (after every renderer action).
+   */
+  DURING_RENDERING = "DURING_RENDERING",
+
+  /**
+   * Resolves variables after finale rendering has been completed.
+   */
+  POST_FINAL_RENDERING = "POST_RENDERING",
+}
+
+/**
+ * Nft Generation varibale object
+ */
+export interface NftGenerationVariable {
+  /**
+   * variable name to be searched. If the name is "VAR", the system will search
+   * for "{{ VAR }}", in order to substitute it with the corresponding content
+   */
+  name: string;
+
+  /**
+   * variable content to be substituted
+   */
+  content: string;
+
+  /**
+   * Specifies when does the resolution has to take place (after rendering, during rendering, etc...)
+   */
+  resolutionStage: NftGenerationResolutionStage;
+}
+
 export interface IGenerationDataClientModel extends IBaseModel {
   /**
    * NftGeneration ID
@@ -43,6 +79,11 @@ export interface IGenerationDataClientModel extends IBaseModel {
    * Other attributes
    */
   attributes: { [key: string]: any };
+
+  /**
+   * Nft Generation Variables
+   */
+  variables: Array<NftGenerationVariable>;
 }
 
 @JsonObject()
@@ -71,6 +112,14 @@ export class GenerationDataClientModel
   @JsonProperty()
   attributes: { [key: string]: any };
 
+  @JsonProperty()
+  variables: Array<NftGenerationVariable>;
+
+  constructor() {
+    super();
+    this.variables = [];
+  }
+
   /**
    *
    * @returns The raw version of generation data, that can
@@ -84,6 +133,7 @@ export class GenerationDataClientModel
       resourceOwner: this.resourceOwner,
       usersCanReveal: this.usersCanReveal,
       supportsReveal: this.supportsReveal,
+      variables: this.variables,
     };
 
     return Object.assign(object, this.attributes);
